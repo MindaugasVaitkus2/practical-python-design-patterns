a083db8abdb453f9cbb0e194653746429af0dd47
Jon Boone <jon_boone@cable.comcast.com>
2017-12-19 16:55:56 -0500

    added Rogue unit_type

--- prototype_pattern/rts_prototype.py
+++ prototype_pattern/rts_prototype.py
@@ -45,6 +45,28 @@ class Archer(Prototype):
     def clone(self):
         return deepcopy(self)
 
+class Rogue(Prototype):
+    def __init__(self, level):
+        self.unit_type = "Rogue"
+
+        filename = "{}_{}.dat".format(self.unit_type, level)
+
+        with open(filename, 'r') as parameter_file:
+            lines = parameter_file.read().split("\n")
+            self.life = lines[0]
+            self.speed = lines[1]
+            self.attack_power = lines[2]
+            self.attack_range = lines[3]
+            self.weapon = lines[4]
+
+    def __str__(self):
+        return "Type: {0}\nLife: {0}\nSpeed: {2}\nAttack Power: {3}\nAttack Range: {4}\nWeapon: {5}".format(
+            self.unit_type, self.life, self.speed, self.attack_power, self.attack_range, self.weapon
+        )
+
+    def clone(self):
+        return deepcopy(self)
+
 class Barracks(object):
     def __init__(self):
         self.units = {
@@ -55,6 +77,10 @@ class Barracks(object):
             "archer": {
                 1: Archer(1),
                 2: Archer(2)
+            },
+            "rogue": {
+                1: Rogue(1),
+                2: Rogue(2)
             }
         }
 
@@ -65,5 +91,7 @@ if __name__ == "__main__":
     barracks = Barracks()
     knight1 = barracks.build_unit("knight", 1)
     archer1 = barracks.build_unit("archer", 2)
+    rogue1 = barracks.build_unit("rogue", 1)
     print("[knight1] {}".format(knight1))
     print("[archer1] {}".format(archer1))
+    print("[rogue1] {}".format(rogue1))
