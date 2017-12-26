72ada98afeacbb894964758493a3039d9f17dfbe
Jon Boone <ipmonger@delamancha.org>
2017-12-26 07:56:23 -0500

    updated to keep block inside window boundaries

--- factory_pattern/shape_game.py
+++ factory_pattern/shape_game.py
@@ -14,10 +14,10 @@ while not player_quits:
             player_quits = True
 
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 4
-        if pressed[pygame.K_DOWN]: y += 4
-        if pressed[pygame.K_LEFT]: x -= 4
-        if pressed[pygame.K_RIGHT]: x += 4
+        if pressed[pygame.K_UP] and y > 4: y -= 4
+        if pressed[pygame.K_DOWN] and y < 576: y += 4
+        if pressed[pygame.K_LEFT] and x > 4: x -= 4
+        if pressed[pygame.K_RIGHT] and x < 776: x += 4
 
         screen.fill((0, 0, 0))
         pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(x, y, 20, 20))
