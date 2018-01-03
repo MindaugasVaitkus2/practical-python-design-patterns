cddbd96023bce3e03abd132ab7ea4a4ab7c4d183
Jon Boone <ipmonger@delamancha.org>
2018-01-03 10:09:58 -0500

    corrected typo in map() statement

--- builder_pattern/html_form_generator.py
+++ builder_pattern/html_form_generator.py
@@ -1,8 +1,9 @@
 def generate_webform(field_list):
     generated_fields = "\n".join(
-        map()
+        map(
             lambda x: '{0}:<br><input type="text" name="{0}"><br>'.format(x),
             field_list
+        )
     )
     return "<form>{fields}</form>".format(fields=generated_fields)
 
