1c19b375396c30411f24db430cec7697f4641dfb
Jon Boone <ipmonger@delamancha.org>
2018-01-03 10:10:38 -0500

    corrected typo in equivalence checking

--- builder_pattern/html_form_generator.py
+++ builder_pattern/html_form_generator.py
@@ -13,6 +13,6 @@ def build_html_form(fields):
             "<html><body>{}</body></html>".format(generate_webform(fields))
         )
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     fields = ["name", "age", "email", "telephone"]
     build_html_form(fields)
