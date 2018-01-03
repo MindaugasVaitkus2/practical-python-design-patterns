dbef258efaaf39271d04610ca919f66349a3939e
Jon Boone <ipmonger@delamancha.org>
2018-01-03 10:22:21 -0500

    expanded to support both text input fields and checkbox input fields

--- builder_pattern/html_form_generator.py
+++ builder_pattern/html_form_generator.py
@@ -1,18 +1,30 @@
-def generate_webform(field_list):
+def generate_webform(text_field_list=[], checkbox_field_list=[]):
     generated_fields = "\n".join(
         map(
             lambda x: '{0}:<br><input type="text" name="{0}"><br>'.format(x),
-            field_list
+            text_field_list
+        )
+    )
+
+    generated_fields += "\n".join(
+        map(
+            lambda x: '<label><input type="checkbox" id="{0}" value="{0}">{0}<br>'.format(x),
+            checkbox_field_list
         )
     )
     return "<form>{fields}</form>".format(fields=generated_fields)
 
-def build_html_form(fields):
+def build_html_form(text_field_list=[], checkbox_field_list=[]):
     with open("form_file.hmtl", 'w') as f:
         f.write(
-            "<html><body>{}</body></html>".format(generate_webform(fields))
+            "<html><body>{}</body></html>".format(
+                generate_webform(
+                    text_field_list=text_field_list,
+                    checkbox_field_list=checkbox_field_list
+                )
         )
 
 if __name__ == "__main__":
-    fields = ["name", "age", "email", "telephone"]
-    build_html_form(fields)
+    text_fields = ["name", "age", "email", "telephone"]
+    checkbox_fields = ["awesome", "bad"]
+    build_html_form(text_field_list=text_fields, checkbox_field_list=checkbox_fields)
